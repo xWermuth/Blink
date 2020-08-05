87dbe643f52573e193f1e8fd974079fc0fa4bce3
alwe <alwe@itu.dk>
2020-08-05 22:09:12 +0200

    Add reselect modules

--- client/src/pages/Test.tsx
+++ client/src/pages/Test.tsx
@@ -1,23 +1,22 @@
-import React, { Fragment, useState, useEffect } from "react";
+import React, { Fragment } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { increment, decrement } from "../store/test/test.actions";
+import { getCount } from "../store/test/test.selectors";
 
 export const Test: React.FC = () => {
   const dispatch = useDispatch();
   const handleIncrement = () => {
-    console.log("--incrementing");
     dispatch(increment());
   };
 
   const handleDecrement = () => {
-    console.log("--decrementing");
     dispatch(decrement());
   };
   return (
     <Fragment>
       <button onClick={handleIncrement}>Increment</button>
       <button onClick={handleDecrement}>Decrement</button>
-      <h1>number</h1>
+      <h1>{useSelector(getCount)}</h1>
     </Fragment>
   );
 };
--- client/src/store/test/test.selectors.ts
+++ client/src/store/test/test.selectors.ts
@@ -1,1 +1,6 @@
-export const abe = () => {};
+import { AppState } from "../rootReducer";
+import { createSelector } from "reselect";
+
+const countState = (state: AppState) => state.count;
+
+export const getCount = createSelector(countState, ({ count }) => count);
