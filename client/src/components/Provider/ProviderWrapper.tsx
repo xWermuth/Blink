dcd484bb911a183e18f4bf10b44119180acfa718
alwe <alwe@itu.dk>
2020-08-05 12:03:40 +0200

    Refactored app

---
+++ client/src/components/Provider/ProviderWrapper.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+// interface ProviderWrapper {
+//   store?: string; //TODO change to reac-redux store
+// }
+
+export const ProviderWrapper: React.FC = () => {
+  return <h3>Provider</h3>;
+};
